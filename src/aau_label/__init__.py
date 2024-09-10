bd55467b7ac898c6773c3cfa1c430a8578f571b6
Kasper Fromm Pedersen <kasperfromm@protonmail.com>
2024-09-10 12:24:05 +0200

    Added documentation and improved robustnes

--- src/aau_label/__init__.py
+++ src/aau_label/__init__.py
@@ -4,11 +4,12 @@ import logging
 from datetime import datetime
 from pathlib import Path
 from typing import Any, Iterable, Sequence
+from xml.etree.ElementTree import ParseError as XMLParseError
 
 from pandas import DataFrame
 
 from . import utilities
-from .errors import LabelError
+from .errors import ClassFileNotFoundError, LabelError
 from .io import Darknet, Pascal
 from .model import AAULabel, AAULabelImage, COCOInfo, COCOLicense
 from .protocols import Label, LabelImage, LabelImageDeserializer
@@ -17,6 +18,15 @@ logger = logging.getLogger(__name__)
 
 
 def from_file(filepath: str | Path) -> list[LabelImage]:
+    """
+    Loads a list of LabelImage objects from a AAU JSON file.
+
+    Args:
+        filepath (str | Path): The path to the AAU JSON file to load from.
+
+    Returns:
+        list[LabelImage]: A list of LabelImage objects loaded from the file.
+    """
     label_images = []
     with open(filepath, "r") as file:
         data = json.load(file)
@@ -68,6 +78,17 @@ def from_dir(
     label_dir: str | Path,
     deserializer: LabelImageDeserializer,
 ) -> Iterable[LabelImage]:
+    """
+    Generate an iterable of LabelImage objects from a directory containing images and labels.
+
+    Args:
+        img_dir: The directory containing the images.
+        label_dir: The directory containing the labels.
+        deserializer: A LabelImageDeserializer instance used to deserialize the label files.
+
+    Returns:
+        An iterable of LabelImage objects.
+    """
     if isinstance(img_dir, str):
         img_dir = Path(img_dir)
     if isinstance(label_dir, str):
@@ -83,26 +104,69 @@ def from_dir(
     for img_path, label_path in pairs:
         try:
             yield deserializer.deserialize(img_path, label_path)
-        except (FileNotFoundError, ValueError, LabelError) as error:
-            logger.warning(error)
+        except (FileNotFoundError, ValueError, LabelError, XMLParseError) as error:
+            logger.error(error)
 
 
 def from_pascal_dir(img_dir: str | Path, label_dir: str | Path) -> Iterable[LabelImage]:
+    """
+    Generate an iterable of LabelImage objects from a directory containing images and labels in Pascal VOC format.
+
+    Args:
+        img_dir: The directory containing the images.
+        label_dir: The directory containing the labels.
+
+    Returns:
+        An iterable of LabelImage objects.
+    """
     return from_dir(img_dir, label_dir, Pascal(label_dir))
 
 
 def from_darknet_dir(
-    img_dir: str | Path, label_dir: str | Path
+    img_dir: str | Path,
+    label_dir: str | Path,
+    class_file: str | Path | None = None,
 ) -> Iterable[LabelImage]:
+    """
+    Generate an iterable of LabelImage objects from a directory containing images and labels in Darknet format.
+
+    Args:
+        img_dir (str | Path): The directory containing the images.
+        label_dir (str | Path): The directory containing the labels.
+        class_file (str | Path | None, optional): The path to the class file. If not provided, it will be inferred from the label directory.
+
+    Returns:
+        Iterable[LabelImage]: An iterable of LabelImage objects.
+    """
     if isinstance(label_dir, str):
         label_dir = Path(label_dir)
+    if isinstance(class_file, str):
+        class_file = Path(class_file)
+    if class_file is None:
+        candidates = ["classes.txt", "names.txt", ".names"]
 
-    class_file = label_dir.joinpath("classes.txt")
+        for candidate in candidates:
+            class_file = label_dir.joinpath(candidate)
+            if class_file.exists():
+                break
+        else:
+            raise ClassFileNotFoundError(label_dir)
+
     classes = Darknet.load_class_file(class_file)
     return from_dir(img_dir, label_dir, Darknet(classes))
 
 
 def write(filepath: str | Path, label_images: Iterable[LabelImage]) -> None:
+    """
+    Writes a list of LabelImage objects to a file in AAU JSON format.
+
+    Args:
+        filepath (str | Path): The path to the file where the LabelImage objects will be written.
+        label_images (Iterable[LabelImage]): The list of LabelImage objects to be written.
+
+    Returns:
+        None
+    """
     label_image_dicts = map(utilities.label_image_to_dict, label_images)
     with open(filepath, "w") as file:
         json.dump(list(label_image_dicts), file, indent=2)
@@ -118,6 +182,17 @@ def __get_unique_classifiers(label_images: Iterable[LabelImage]) -> dict[str, in
 def to_coco(
     label_images: Iterable[LabelImage], license: COCOLicense, info: COCOInfo
 ) -> dict[str, Any]:
+    """
+    Converts a list of LabelImage objects into a COCO dataset dictionary.
+
+    Args:
+        label_images (Iterable[LabelImage]): A list of LabelImage objects.
+        license (COCOLicense): The license information for the dataset.
+        info (COCOInfo): The info section of the COCO dataset.
+
+    Returns:
+        dict[str, Any]: A dictionary representing the COCO dataset.
+    """
     logger.info("Extracting unique labels from LabelImage objects")
     classifier_db = __get_unique_classifiers(label_images)
 
@@ -171,6 +246,15 @@ def to_coco(
 
 
 def to_dataframe(label_images: Sequence[LabelImage]) -> DataFrame:
+    """
+    Converts a sequence of LabelImage objects into a pandas DataFrame.
+
+    Parameters:
+    label_images (Sequence[LabelImage]): A sequence of LabelImage objects.
+
+    Returns:
+    DataFrame: A pandas DataFrame containing the label image data.
+    """
     rows = []
     for label_image in label_images:
         rows.extend(
@@ -190,6 +274,16 @@ def to_dataframe(label_images: Sequence[LabelImage]) -> DataFrame:
 
 
 def from_dataframe(df: DataFrame) -> Iterable[LabelImage]:
+    """
+    Converts a pandas DataFrame with the columns ["path", "image_width", "image_height", "x", "y", "label_width", "label_height", "classifier"]
+    into an iterable of LabelImage objects.
+
+    Parameters:
+    df (DataFrame): A pandas DataFrame containing image data.
+
+    Returns:
+    Iterable[LabelImage]: An iterable of LabelImage objects.
+    """
     group_by = df.groupby(["path", "image_width", "image_height"])
     for (path, width, height), group in group_by:  # type: ignore
         labels: Sequence[Label] = [
